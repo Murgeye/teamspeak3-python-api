95a9c1a52edf11882db8064a55ca5ddbe3de7dfb
Fabian Ising <f.ising@fh-muenster.de>
2021-04-12 11:28:09 +0200

    Prepare pypi release

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-__author__ = 'fabian'
-__version__ = "0.1.0"
+__author__ = 'Fabian Ising, Daniel Lukats'
+__version__ = "0.9.0"
---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 Fabian Ising <ts3API@murgi.de>
+#
+# Distributed under terms of the MIT license.
+
+import setuptools
+setuptools.setup()
